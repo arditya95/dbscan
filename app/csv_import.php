fe53c5223767465f3c813247877123e48ce1e25a
Arditya Darmawan <putuarditya@gmail.com>
2017-09-03 22:00:30 +0800

    update_09_03_17 add csv to sql

---
+++ app/csv_import.php
@@ -0,0 +1,13 @@
+<?php
+include_once '../setting/koneksi.php';
+if (($handle = fopen("backup\data.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      $data[0]=preg_replace('/[--]/',' ', $data[0]);
+      $data[0]= date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $data[0])));
+      $sql="INSERT INTO tmp (x_point, y_point) VALUES ('$data[0]', '$data[1]')";
+      mysqli_query($con,$sql);
+    }
+    fclose($handle);
+    // mysql_close($con);
+}
+?>
--- phpml.php
+++ phpml.php
@@ -1,11 +1,23 @@
 <?php
-include 'vendor\autoload.php';
+header('Content-Type: application/json');
+require_once 'vendor/autoload.php';
+
 use Phpml\Classification\KNearestNeighbors;
 use Phpml\Clustering\DBSCAN;
+use Phpml\Clustering\KMeans;
+// $samples = [[1, 3], [1, 4], [2, 4], [3, 1], [4, 1], [4, 2]];
+// $labels = ['a', 'a', 'a', 'b', 'b', 'b'];
+//
+// $classifier = new KNearestNeighbors();
+// $classifier->train($samples, $labels);
+//
+// echo $classifier->predict([3, 2]);
+// return 'b'
 
-$samples = [[1, 1], [8, 7], [1, 2], [7, 8], [2, 1], [8, 9]];
+$samples = [[1, 8], [8, 7], [1, 2], [7, 8], [2, 1], [8, 9]];
 
 $dbscan = new DBSCAN($epsilon = 2, $minSamples = 3);
 $dbscan->cluster($samples);
-
- ?>
+print_r($dbscan->cluster($samples));
+// return [0=>[[1, 1], ...], 1=>[[8, 7], ...]]
+?>
